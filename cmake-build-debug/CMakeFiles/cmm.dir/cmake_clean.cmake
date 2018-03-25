6c559b30ccd081bfc194f03992a68046e828c59f
sky9611 <collinbeyond@gmail.com>
2018-03-25 13:03:19 +0200

    Revert "delete cmake-debug"
    This reverts commit 4ea1448b2b6345cd6d8992e2833713f748c0e066.

---
+++ cmake-build-debug/CMakeFiles/cmm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmm.dir/src/main.cpp.obj"
+  "cmm.pdb"
+  "cmm.exe"
+  "cmm.exe.manifest"
+  "libcmm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
