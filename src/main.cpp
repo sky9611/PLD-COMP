3deb204ef68051c8f279b5b556cd7104df1d180b
sky9611 <collinbeyond@gmail.com>
2018-03-26 16:41:07 +0200

    add .dll

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace antlr4;
 int main(int argc, const char* argv[]) {
 
     ANTLRFileStream input("../Test/test1.txt");
-/*    cmmLexer lexer(&input);
+    cmmLexer lexer(&input);
     CommonTokenStream tokens(&lexer);
     tokens.fill();
     for (auto token : tokens.getTokens()) {
@@ -14,6 +14,6 @@ int main(int argc, const char* argv[]) {
     }
     cmmParser parser(&tokens);
     tree::ParseTree *tree = parser.file();
-    std::cout << tree->toStringTree(&parser) << std::endl;*/
+    std::cout << tree->toStringTree(&parser) << std::endl;
     return 0;
 }
