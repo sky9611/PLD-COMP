58b9fc1e5d21710067a65deda496dedc0767160c
hvogel <hugues.vogel@hotmail.fr>
2018-03-30 20:11:11 +0200

    add buildIR to stmtWhile

--- src/structure/Statements/StmtWhile.cpp
+++ src/structure/Statements/StmtWhile.cpp
@@ -11,5 +11,27 @@ StmtWhile::~StmtWhile()
 }
 
 string StmtWhile::buildIR(CFG* cfg)const{
+    BasicBlock* testBlock = new BasicBlock(cfg, "WhileTest");
+    BasicBlock* whileBlock = new BasicBlock(cfg, "WhileContent");
+    BasicBlock* outBlock = new BasicBlock(cfg, "WhileOut");
+
+    outBlock->exit_true = cfg->current_bb->exit_true;
+    outBlock->exit_false = cfg->current_bb->exit_false;
+
+    cfg->current_bb->exit_true = cfg->current_bb->exit_false = testBlock;
+
+    testBlock->exit_true = whileBlock;
+    testBlock->exit_false = outBlock;
+
+    whileBlock->exit_true = whileBlock->exit_false = testBlock;
+
+    cfg->current_bb = testBlock;
+    this->test->buildIR(cfg);
+
+    cfg->current_bb = whileBlock;
+    this->block->buildIR(cfg);
+
+    cfg->current_bb = outBlock;
 
 }
+
