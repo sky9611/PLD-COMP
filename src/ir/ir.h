93bbf4c5ab11266a672cc126c14660e48acdc0ed
hvogel <hugues.vogel@hotmail.fr>
2018-04-03 09:52:04 +0200

    fix compile bug

---
+++ src/ir/ir.h
@@ -0,0 +1,16 @@
+//
+// Created by hvogel on 03/04/18.
+//
+
+#ifndef PLD_COMP_IR_H
+#define PLD_COMP_IR_H
+
+
+namespace ir {
+    static void gen_asm(Program* p, ostream &o){
+
+    }
+};
+
+
+#endif //PLD_COMP_IR_H
