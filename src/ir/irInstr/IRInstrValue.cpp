22ad44ebded5400fdf3fd1db328a19524ea4a816
wth <collinbeyond@gmail.com>
2018-04-03 10:21:21 +0200

    implement IRInstrValue

---
+++ src/ir/irInstr/IRInstrValue.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wth on 2018/4/3.
+//
+
+#include "IRInstrValue.h"
---
+++ src/ir/irInstr/IRInstrValue.h
@@ -0,0 +1,16 @@
+//
+// Created by wth on 2018/4/3.
+//
+
+#ifndef PLD_COMP_IRINSTRVALUE_H
+#define PLD_COMP_IRINSTRVALUE_H
+
+
+
+class IRInstrValue {
+
+};
+
+
+
+#endif //PLD_COMP_IRINSTRVALUE_H
