1d1cd800ff8e88c39e200db84179bc0c652a06b9
wth <collinbeyond@gmail.com>
2018-04-05 00:28:14 +0200

    new test for array on right side

---
+++ Test/Back/testArrayRightSide.c
@@ -0,0 +1,13 @@
+void main(void) {
+    char a[5] = {'a','b','c','d','e'};
+    char a0 = a[0];
+    char a1 = a[0];
+    char a2 = a[0];
+    char a2 = a[0];
+    char a4 = a[0];
+    putchar(a0);
+    putchar(a1);
+    putchar(a2);
+    putchar(a3);
+    putchar(a4);
+}
