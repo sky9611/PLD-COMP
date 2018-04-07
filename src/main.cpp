e0c6a97dcc94d30dd4af7c23d4a29ba1d0a763cc
zuoduzuodu <zuoduzuodu24@gmail.com>
2018-04-07 16:03:28 +0200

    first push de tests

--- src/main.cpp
+++ src/main.cpp
@@ -15,21 +15,29 @@ void error( string msg ) {
 void info() {
     cout << "cmm Compilation tool" << endl;
     cout << "arguments :" << endl;
-    cout << "Please specify a file name in the input arguments." << endl;
-    cout << "     -c           Compilation into a specified file." << endl;
+    cout << "Please specify a file name (extension .cmm) in the input arguments." << endl;
+    cout << "     -c           Compilation into a specified file. Please add the filename after this argument" << endl;
     cout << "     -a           Perform a static analysis of the program." << endl;
-    cout << "     -o           Perform a simple optimization of the program." << endl;
+    cout << "     -o           Perform a simple optimization of the program. (NOT YET IMPLEMENTED)" << endl;
     cout << "     -h           Display the help page." << endl;
 }
 
-string parseArgs( vector<string> args ) {
-    args.erase( args.begin( ));
-    for ( string a : args ) {
-        if ( a != "-a" && a != "-c" && a != "-o" && a != "-h" ) {
-            return a;
+string parseArgs( vector<string> args, bool diffOut ) {
+    if ( !diffOut ) {
+        args.erase( args.begin( ));
+        for ( string a : args ) {
+            if ( a != "-a" && a != "-c" && a != "-o" && a != "-h" ) {
+                return a;
+            }
         }
+        return string( );
     }
-    return string( );
+    else {
+        for ( int i = 1; i < args.size( ) - 1; i++ ) {
+            if ( args[i] == "-c" ) { return args[i + 1]; }
+        }
+        return string( );
+    }
 }
 
 int main( int argc, const char *argv[] ) {
@@ -61,15 +69,18 @@ int main( int argc, const char *argv[] ) {
     bool compilationToFile = find( args.begin( ), args.end( ), "-c" ) != args.end( );
     bool opmitization = find( args.begin( ), args.end( ), "-o" ) != args.end( );
 
-    string fileName = parseArgs( args );
-    if ( fileName == string( )) {
+    string fileIn = parseArgs( args, false );
+    if ( fileIn == string( )) {
         error( "Error : no file name in arguments" );
         return 1;
     }
-    string fileIn = fileName + string( ".cmm" );
-    string fileOut = fileName + string( ".s" );
-    //string fileIn = string("../Test/Back/") + testName + string(".cmm");
-    //string fileOut =string("../Test/Back/") + testName + string(".s");
+    string fileOut;
+    if ( compilationToFile ) { fileOut = parseArgs( args, true ); }
+    else { fileOut = fileIn.substr( 0, fileIn.find_last_of( '.' )) + ".s"; }
+    if ( fileOut == string( )) {
+        error( "Error : no file specified after -c argument." );
+        return 1;
+    }
     ANTLRFileStream input( fileIn );
     cmmLexer lexer( &input );
     CommonTokenStream tokens( &lexer );
@@ -82,20 +93,24 @@ int main( int argc, const char *argv[] ) {
     //auto a = parser.file();
 
     cmmInterpreter *interpreter = new cmmInterpreter( );
+    Program *b;
+    try {
+        b = tree->accept( interpreter );
 
-    Program *b = tree->accept( interpreter );
-
-    //partie analyse
-    if ( staticAnalysis ) {
-        b->performMisuseAnalysis( );
-        b->performUnuseAnalysis( );
+        //partie analyse
+        if ( staticAnalysis ) {
+            b->performMisuseAnalysis( );
+            b->performUnuseAnalysis( );
+        }
+        //partie construction de la RI
+        b->builIR( );
     }
-    //partie construction de la RI
-    b->builIR( );
-
+    catch (const exception &e ) {
+        cerr << "ERROR during runtime : "<< e.what( ) << endl;
+        return 1;
+    }
     ofstream outFile;
     outFile.open( fileOut );
-
 
     ir::gen_asm( cout, testName + string( ".cmm" ), b );
     ir::gen_asm( outFile, testName + string( ".cmm" ), b );
